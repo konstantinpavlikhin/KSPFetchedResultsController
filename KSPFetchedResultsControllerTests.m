aae5861fc1be5612e39355ed0c0dff8e002b242d
Konstantin Pavlikhin <k.pavlikhin@gmail.com>
2015-03-04 17:54:54 +0300

    Add a test bundle that will test the recently created dynamic library.

---
+++ KSPFetchedResultsControllerTests.m
@@ -0,0 +1,40 @@
+//
+//  KSPFetchedResultsControllerTests.m
+//  KSPFetchedResultsController Tests
+//
+//  Created by Konstantin Pavlikhin on 04/03/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface KSPFetchedResultsController_Tests : XCTestCase
+
+@end
+
+@implementation KSPFetchedResultsController_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

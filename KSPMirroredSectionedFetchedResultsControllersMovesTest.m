05ba42d6909480f96afc9db71dcc078dfd62ab17
Konstantin Pavlikhin <k.pavlikhin@gmail.com>
2015-03-05 11:15:00 +0300

    Add a new test case for the Mirrored Sectioned FRC's objects moves.

---
+++ KSPMirroredSectionedFetchedResultsControllersMovesTest.m
@@ -0,0 +1,198 @@
+//
+//  KSPMirroredSectionedFetchedResultsControllersMovesTest.m
+//  KSPFetchedResultsController
+//
+//  Created by Konstantin Pavlikhin on 05/03/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <XCTest/XCTest.h>
+
+// * * *.
+
+#define HC_SHORTHAND
+
+#import <OCHamcrest/OCHamcrest.h>
+
+#define MOCKITO_SHORTHAND
+
+#import <OCMockito/OCMockito.h>
+
+// * * *.
+
+#import "KSPMirroredSectionedFetchedResultsController.h"
+
+#import "KSPSectionedFetchedResultsControllerDelegate.h"
+
+#import "KSPTableSection.h"
+
+static const NSUInteger DummyUnsignedInteger = 888;
+
+@interface KSPMirroredSectionedFetchedResultsControllersMovesTest : XCTestCase
+
+@end
+
+@implementation KSPMirroredSectionedFetchedResultsControllersMovesTest
+{
+  NSManagedObjectContext* _context;
+
+  KSPMirroredSectionedFetchedResultsController* _MSFRC;
+
+  id<KSPSectionedFetchedResultsControllerDelegate> _delegate;
+
+  NSManagedObject *_msg0, *_msg1, *_msg2, *_msg3, *_msg4, *_msg5, *_msg6, *_msg7;
+}
+
++ (NSManagedObjectContext*) managedObjectContextForTests
+{
+  static NSManagedObjectModel* model = nil;
+
+  if(!model)
+  {
+    NSURL* URL = [[NSBundle bundleForClass: self] URLForResource: @"TestModel" withExtension: @"mom"];
+
+    model = [[NSManagedObjectModel alloc] initWithContentsOfURL: URL];
+  }
+
+  static NSManagedObjectContext* MOC = nil;
+
+  if(!MOC)
+  {
+    NSPersistentStoreCoordinator* PSC = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel: model];
+
+    NSPersistentStore* store = [PSC addPersistentStoreWithType: NSInMemoryStoreType configuration: nil URL: nil options: nil error: nil];
+
+    NSAssert(store, @"Should have a store by now.");
+
+    MOC = [[NSManagedObjectContext alloc] initWithConcurrencyType: NSMainQueueConcurrencyType];
+
+    MOC.persistentStoreCoordinator = PSC;
+
+    MOC.undoManager = nil;
+  }
+
+  return MOC;
+}
+
++ (NSManagedObject*) messageWithSequenceNumber: (NSUInteger) sequenceNumber text: (NSString*) text date: (NSDate*) date
+{
+  NSManagedObjectContext* MOC = [self managedObjectContextForTests];
+
+  NSEntityDescription* entityDescription = [NSEntityDescription entityForName: @"Message" inManagedObjectContext: MOC];
+
+  NSManagedObject* message = [[NSManagedObject alloc] initWithEntity: entityDescription insertIntoManagedObjectContext: nil];
+
+  [message setValue: @(sequenceNumber) forKey: @"sequenceNumber"];
+
+  [message setValue: text forKey: @"text"];
+
+  [message setValue: date forKey: @"date"];
+  
+  return message;
+}
+
+- (void) setUp
+{
+  [super setUp];
+
+  // * * *.
+
+  _context = [[self class] managedObjectContextForTests];
+
+  NSDate* date = [NSDate date];
+
+  _msg0 = [[self class] messageWithSequenceNumber: 0 text: @"Here's to the crazy ones." date: date];
+
+  [_context insertObject: _msg0];
+
+  _msg1 = [[self class] messageWithSequenceNumber: 10 text: @"The misfits." date: date];
+
+  [_context insertObject: _msg1];
+
+  _msg2 = [[self class] messageWithSequenceNumber: 20 text: @"The rebels." date: date];
+
+  [_context insertObject: _msg2];
+
+  _msg3 = [[self class] messageWithSequenceNumber: 30 text: @"The troublemakers." date: date];
+
+  [_context insertObject: _msg3];
+
+  _msg4 = [[self class] messageWithSequenceNumber: 40 text: @"The round pegs in the square holes." date: date];
+
+  [_context insertObject: _msg4];
+
+  _msg5 = [[self class] messageWithSequenceNumber: 50 text: @"The ones who see things differently." date: date];
+
+  [_context insertObject: _msg5];
+
+  _msg6 = [[self class] messageWithSequenceNumber: 60 text: @"They're not fond of rules." date: date];
+
+  [_context insertObject: _msg6];
+
+  _msg7 = [[self class] messageWithSequenceNumber: 70 text: @"And they have no respect for the status quo." date: date];
+
+  [_context insertObject: _msg7];
+
+  // * * *.
+
+  [_context processPendingChanges];
+
+  // * * *.
+
+  NSFetchRequest* fetchRequest = [[NSFetchRequest alloc] initWithEntityName: @"Message"];
+
+  fetchRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey: @"sequenceNumber" ascending: YES]];
+
+  // * * *.
+
+  _MSFRC = [[KSPMirroredSectionedFetchedResultsController alloc] initWithFetchRequest: fetchRequest managedObjectContext: _context sectionNameKeyPath: @"date"];
+
+  // * * *.
+
+  _delegate = mockProtocol(@protocol(KSPSectionedFetchedResultsControllerDelegate));
+
+  _MSFRC.delegate = _delegate;
+
+  NSError* error;
+
+  if(![_MSFRC performFetch: &error])
+  {
+    NSLog(@"%@", error);
+  }
+
+  // * * *.
+
+  XCTAssert(_MSFRC.mirroredFetchedObjects.count == 8, @"fetchedObjects count should be 8.");
+}
+
+- (void) tearDown
+{
+  [[[self class] managedObjectContextForTests] reset];
+
+  // * * *.
+
+  [super tearDown];
+}
+
+- (void) testInnerMove
+{
+  [_msg3 setValue: @44 forKey: @"sequenceNumber"];
+
+  [_context processPendingChanges];
+
+  MKTArgumentCaptor* argumentCaptor = [[MKTArgumentCaptor alloc] init];
+
+  [[verify(_delegate) withMatcher: [argumentCaptor capture] forArgument: 5] controller: _MSFRC willChangeObject: _msg3 atIndex: 4 inSection: instanceOf([KSPTableSection class]) forChangeType: KSPFetchedResultsChangeMove newIndex: DummyUnsignedInteger inSection: instanceOf([KSPTableSection class])];
+
+  NSLog(@"————> %@", [argumentCaptor value]);
+
+  // * * *.
+
+  [verify(_delegate) controller: _MSFRC willChangeObject: _msg3 atIndex: 4 inSection: instanceOf([KSPTableSection class]) forChangeType: KSPFetchedResultsChangeMove newIndex: 3 inSection: instanceOf([KSPTableSection class])];
+
+  [verify(_delegate) controller: _MSFRC didChangeObject: _msg3 atIndex: 4 inSection: instanceOf([KSPTableSection class]) forChangeType: KSPFetchedResultsChangeMove newIndex: 3 inSection: instanceOf([KSPTableSection class])];
+}
+
+@end
